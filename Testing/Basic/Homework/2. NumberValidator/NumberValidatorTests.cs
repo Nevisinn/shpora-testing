79dbb0014df0f155c6b964c9888c98878eed15ce
Nevisinn <igor.shub93@mail.ru>
2025-10-29 16:33:35 +0500

    Добавление и исправление тестов

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -6,61 +6,57 @@ namespace HomeExercise.Tasks.NumberValidator;
 [TestFixture]
 public class NumberValidatorTests
 {
-    [TestCase(1, 0, true, TestName = "OnlyPositiveIsTrue")]
-    [TestCase(1, 0, false, TestName = "OnlyPositiveIsFalse")] 
-    [TestCase(int.MaxValue, 0, false, TestName = "PrecisionMaxValue")] 
-    public void Constructor_WhenValidArguments_ReturnVoid(int precision, int scale, bool onlyPositive)
-    {   
-        var func = () => new NumberValidator(precision, scale, onlyPositive);
-
-        func.Should().NotThrow();
-    }
-    
     [TestCase(-1, 2, false, TestName = "PrecisionIsNegative")]  
     [TestCase(1, 2, false, TestName = "ScaleGreaterThanPrecision")]
     [TestCase(1, 1, false, TestName = "ScaleEqualsPrecision")]  
     [TestCase(1, -1, false, TestName = "NegativeScale")] 
+    [TestCase(0, 2, false, TestName = "ZeroPrecision")]
     public void Constructor_WithInvalidArguments_ThrowsArgumentException(int precision, int scale, bool onlyPositive)
     {   
-        var func = () => new NumberValidator(precision, scale, onlyPositive);
+        var createNumberValidator = () => new NumberValidator(precision, scale, onlyPositive);
 
-        func.Should().Throw<ArgumentException>();
+        createNumberValidator.Should().Throw<ArgumentException>();
     }
     
-    [TestCase(30, 2, true, "0.0", ExpectedResult = true, TestName = "DecimalValue")]
-    [TestCase(30, 2, true, "0", ExpectedResult = true, TestName = "OneDigitValue")]
-    [TestCase(30, 2, true,"00.00", ExpectedResult = true, TestName = "MultipleDigitsInWholePart")]
-    [TestCase(3, 2, true, "+1.23", ExpectedResult = false, TestName = "PositiveValue")]
-    [TestCase(4, 2, true, "-1.23", ExpectedResult = false, TestName = "NegativeValue")]
-    [TestCase(6, 3, true, "-1 .2 3", ExpectedResult = false, TestName = "NumberWithEmptySpace")]
-    [TestCase(30, 2, true, "922337203685477580711.0", ExpectedResult = true, TestName = "VeryLargeWholePartNumber")]
-    [TestCase(31, 30, true, "0.922337203685477580711", ExpectedResult = true, TestName = "VeryLargeFractionalPartNumber")]
-    public bool IsValidNumber_WithValidArguments_ReturnsTrue(int precision, int scale, bool onlyPositive, string value)
+    [TestCase(30, 2, true, "0.0", TestName = "DecimalValue")]
+    [TestCase(30, 2, true, "0,0", TestName = "DecimalValueWithComma")]
+    [TestCase(30, 2, true, "0", TestName = "OneDigitValue")]
+    [TestCase(30, 2, true,"00.00", TestName = "MultipleDigitsInWholePart")]
+    [TestCase(30, 2, true, "922337203685477580711.0", TestName = "VeryLargeWholePartNumber")]
+    [TestCase(31, 30, true, "0.922337203685477580711", TestName = "VeryLargeFractionalPartNumber")]
+    [TestCase(30, 2, false, "-0.0", TestName = "NegativeValueWithOnlyPositiveFalse")]
+    public void IsValidNumber_WithValidArguments_ReturnsTrue(int precision, int scale, bool onlyPositive, string value)
     {
         var numberValidator = new NumberValidator(precision, scale, onlyPositive);
         
-        return numberValidator.IsValidNumber(value);
+        var isValidNumber = numberValidator.IsValidNumber(value);
+
+        isValidNumber.Should().BeTrue();
     }
     
-    [TestCase(3, 2, true, "-0.00", ExpectedResult = false, TestName = "PrecisionLessThanValueLength")]
-    [TestCase(30, 2, true, "0.922", ExpectedResult = false, TestName = "FractionalPartNumberGreaterScale")]
-    [TestCase(30, 2, true, "a.sd", ExpectedResult = false, TestName = "Letters")]
-    [TestCase(30, 2, true, "1.sd", ExpectedResult = false, TestName = "MixedNumericAndLetters")]
-    [TestCase(30, 2, true, "1.\n", ExpectedResult = false, TestName = "MixedNumericAndControlCharacter")]
-    [TestCase(4, 2, true, "+\n.10", ExpectedResult = false, TestName = "MixedNumericAndPositiveLeadingControlCharacter")]
-    [TestCase(30, 2, true, "🛠.🛠🛠", ExpectedResult = false, TestName = "HTMLSymbols")]
-    [TestCase(30, 2, true, "", ExpectedResult = false, TestName = "EmptyValue")]
-    [TestCase(30, 2, true, null, ExpectedResult = false, TestName = "NullValue")]
-    [TestCase(30, 2, true, "Hello world!", ExpectedResult = false, TestName = "Text")]
-    [TestCase(30, 2, true, "0+0", ExpectedResult = false, TestName = "InvalidSignPosition")]
-    [TestCase(4, 2, true, "+-0.0", ExpectedResult = false, TestName = "DuplicateSigns")]
-    [TestCase(30, 2, true, "+++", ExpectedResult = false, TestName = "MultipleSigns")]
-    [TestCase(30, 2, true, "   ", ExpectedResult = false, TestName = "WhitespaceOnly")]
-    [TestCase(30, 2, true, ".12", ExpectedResult = false, TestName = "MissingIntegerPart")]
-    public bool IsValidNumber_WithInvalidArguments_ReturnsFalse(int precision, int scale, bool onlyPositive, string value)
+    [TestCase(3, 2, true, "00.00", TestName = "PrecisionLessThanValueLength")]
+    [TestCase(30, 2, true, "0.922", TestName = "FractionalPartNumberGreaterScale")]
+    [TestCase(30, 2, true, "a.sd", TestName = "Letters")]
+    [TestCase(30, 2, true, "1.sd", TestName = "MixedNumericAndLetters")]
+    [TestCase(30, 2, true, "1.\n", TestName = "MixedNumericAndControlCharacter")]
+    [TestCase(4, 2, true, "+\n.10", TestName = "MixedNumericAndPositiveLeadingControlCharacter")]
+    [TestCase(30, 2, true, "🛠.🛠🛠", TestName = "HTMLSymbols")]
+    [TestCase(30, 2, true, "", TestName = "EmptyValue")]
+    [TestCase(30, 2, true, null, TestName = "NullValue")]
+    [TestCase(30, 2, true, "Hello world!", TestName = "Text")]
+    [TestCase(30, 2, true, "0+0", TestName = "InvalidSignPosition")]
+    [TestCase(4, 2, true, "+-0.0", TestName = "DuplicateSigns")]
+    [TestCase(30, 2, true, "+++", TestName = "MultipleSigns")]
+    [TestCase(30, 2, true, "   ", TestName = "WhitespaceOnly")]
+    [TestCase(30, 2, true, ".12", TestName = "MissingIntegerPart")]
+    [TestCase(4, 2, true, "-1.23", TestName = "NegativeValueWithOnlyPositive")]
+    [TestCase(6, 3, true, "+1 .2 3", TestName = "NumberWithEmptySpace")]
+    public void IsValidNumber_WithInvalidArguments_ReturnsFalse(int precision, int scale, bool onlyPositive, string value)
     {   
         var numberValidator = new NumberValidator(precision, scale, onlyPositive);
-        
-        return numberValidator.IsValidNumber(value);
+
+        var isValidNumber = numberValidator.IsValidNumber(value);
+
+        isValidNumber.Should().BeFalse();
     }
 }
